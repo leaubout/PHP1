6b54f71e8f9ab1389d2630210568739e17bc983a
P1 <leaubout@gmail.com>
2014-10-16 11:46:06 +0200

    Prise en compte de l'éventuelle absence de titre défini dans le body

--- src/layout/header.php
+++ src/layout/header.php
@@ -4,17 +4,15 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?= $title; ?></title>
+        <title><?php if (isset($title)){ echo $title; }?></title>
 
         <!-- Bootstrap -->
-        <link href="assets/bootstrap/dist/css/bootstrap.min.css"
-	       rel="stylesheet">
-        <link href="assets/bootflatv2/bootflat/css/bootflat.min.css"
-	       rel="stylesheet">
-        <link href="css/app.css"
-	       rel="stylesheet">	       
+        <link href="assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/bootflatv2/bootflat/css/bootflat.min.css" rel="stylesheet">
         <?php if (isset($afterBootstrap)) {echo $afterBootstrap;}?>
-
+        
+        <link href="css/app.css" rel="stylesheet">	       
+        
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
@@ -24,3 +22,4 @@
     </head>
     <body>
     <?php require_once '../layout/menu.php';?>    
+
